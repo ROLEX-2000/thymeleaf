abb97edb63c2e147b6fe86e0e2b654dc3e2cfaf9
Peddada Jaya Venkata Pavan <peddada.jayav@hcl.com>
2023-02-02 18:52:48 +0530

    first commit

---
+++ src/main/java/com/crud/javashastra/repository/BookRepository.java
@@ -0,0 +1,11 @@
+//package com.crud.javashastra.respository;
+package com.crud.javashastra.repository; 
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.crud.javashastra.entity.Book;
+
+public interface BookRepository extends CrudRepository<Book, Integer> {
+
+}
+
